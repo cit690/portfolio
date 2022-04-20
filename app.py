83b38c150a9ad7c1cdda8de79c172fa3eac7aad8
Claire Thompson <clairethompson@Claires-MacBook-Pro.local>
2022-04-20 13:46:57 -0400

    css and main page layout

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, url_for
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', methods=['GET'])
 
 @app.route('/debug/<thing>')
 def debug_test(thing):
